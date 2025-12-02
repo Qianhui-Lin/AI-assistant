854b54b96845bef4b8b69e6433a55fb918ef324b
Qianhui-Lin <qianhuilin33@gmail.com>
2025-12-02 22:57:52 +0000

    included in-memory history store

---
+++ app/helper/history_store.py
@@ -0,0 +1,13 @@
+from collections import defaultdict, deque
+import os
+HISTORY_LIMIT = os.getenv("HISTORY_LIMIT", 20)
+HISTORY = defaultdict(lambda: deque(maxlen=HISTORY_LIMIT))
+
+def add_history(token: str, question: str, answer: str):
+    HISTORY[token].append({
+        "question": question,
+        "answer": answer
+    })
+
+def get_history(token: str):
+    return list(HISTORY[token])
